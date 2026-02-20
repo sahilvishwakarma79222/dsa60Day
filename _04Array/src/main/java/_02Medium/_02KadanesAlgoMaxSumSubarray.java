3e69eeb7960cbd1ca07ca6703aa412baf193932c
sahilvishwakarma79222 <vsahil79222@gmail.com>
2026-02-20 11:11:21 +0530

    max sub array kadanes algorithm

---
+++ _04Array/src/main/java/_02Medium/_02KadanesAlgoMaxSumSubarray.java
@@ -0,0 +1,29 @@
+package _02Medium;
+
+public class _02KadanesAlgoMaxSumSubarray {
+
+    public static int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int overAllSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (currentSum >= 0) {
+                currentSum += nums[i];
+            } else {
+                currentSum = nums[i];
+            }
+            if (currentSum > overAllSum) {
+                overAllSum = currentSum;
+            }
+        }
+        return overAllSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int num = maxSubArray(nums);
+        System.out.println(num);
+    }
+
+}
