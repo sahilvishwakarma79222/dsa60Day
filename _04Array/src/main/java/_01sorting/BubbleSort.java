42fbf5f5164ea819da16678f874bcdd344238e9a
sahilvishwakarma79222 <vsahil79222@gmail.com>
2026-08-11 18:08:55 +0530

    bubble sort

--- _04Array/src/main/java/_01sorting/BubbleSort.java
+++ _04Array/src/main/java/_01sorting/BubbleSort.java
@@ -2,6 +2,15 @@ package _01sorting;
 
 public class BubbleSort {
 
+    public static void bubbleSort(int[] nums){
+        int n=nums.length;
+        boolean swapped;
+
+        for(int i=0;i<nums.length-1;i++){
+            
+        }
+
+    }
 
     public static void main(String[] args) {
        
