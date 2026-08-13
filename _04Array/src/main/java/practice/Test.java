23acab56f1021699986d15c7a51a156e97dccdf4
sahil <vsahil79222@gmail.com>
2026-08-13 22:53:37 +0530

    bubble sort done practice

--- _04Array/src/main/java/practice/Test.java
+++ _04Array/src/main/java/practice/Test.java
@@ -2,39 +2,37 @@ package practice;
 
 public class Test {
 
+
     public static void main(String[] args) {
-        int[] nums = { 5, 6, 4, 3, 4, 5, 6, 7, 7, 6, 5, 5, 4, 4, 5, 6, 7 };
+
+        int[] nums={2,3,5,76,8,4,3,2};
         print(nums);
+        System.out.println("========================");
         bubbleSort(nums);
-        System.out.println("====================");
         print(nums);
     }
 
-    public static void bubbleSort(int[] arr) {
 
-        int n = arr.length;
+    public static void bubbleSort(int[] nums){
+        int n=nums.length;
         boolean swapped;
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] < arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
+        for(int i=0;i<n-1;i++){
+            swapped=false;
+            for(int j=0;j<n-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                    swapped=true;
                 }
             }
-            if (swapped = false) {
-                break;
-            }
-        }
-
-    }
-
-    public static void print(int[] nums) {
-        for (int n : nums) {
-            System.out.print(n + " ");
+            if(!swapped)break;
         }
     }
 
+    public static void print(int[] nums){
+        for(int n:nums){
+            System.out.print(n+" ");
+        }
+    }
 }
